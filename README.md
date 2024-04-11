45a5ba48c5d5fed2e2598a496b750c191b42421c
Sulie0106 <166704005+Sulie0106@users.noreply.github.com>
2024-04-11 11:15:00 +0200

    Initiële bestanden voor MakeCode-project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open deze pagina op [https://sulie0106.github.io/sulie/](https://sulie0106.github.io/sulie/)
+
+## Gebruiken als extensie
+
+Deze repository kan worden toegevoegd als **extensie** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klik op **Nieuw project**
+* klik op **Extensies** onder het tandwielmenu
+* zoeken naar **https://github.com/sulie0106/sulie** en importeren
+
+## Dit project bewerken
+
+Om deze repository te bewerken in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klik op **Importeren** en klik vervolgens op **Importeer URL**
+* plak **https://github.com/sulie0106/sulie** en klik op importeren
+
+#### Metadata (gebruikt voor zoeken, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,56 @@
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (false) {
+    	
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    number = randint(1, 6)
+})
